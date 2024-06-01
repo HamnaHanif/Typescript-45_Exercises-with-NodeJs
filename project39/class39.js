83a34da7f10e01be453d69cf8ddb6d43d69bc776
HamnaHanif <hanifhamna5@gmail.com>
2024-06-01 15:32:46 -0700

    Add files via upload

---
+++ project39/class39.js
@@ -0,0 +1,6 @@
+// Calling a function with parameters which return a value in string
+function city_country(city, country) {
+    return "".concat(city, " , ").concat(country);
+}
+// Calling a function and print the returned value
+console.log(city_country("Karachi", "Pakistan"));
