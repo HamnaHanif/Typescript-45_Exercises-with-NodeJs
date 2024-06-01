bfc7b28766406c870c7b0a5fa6ad17c65f8f5f25
HamnaHanif <hanifhamna5@gmail.com>
2024-06-01 15:26:21 -0700

    Add files via upload

---
+++ project29/class29.js
@@ -0,0 +1,17 @@
+"use strict";
+let favorite_fruits = ["Mango", "Strawberry", "Orange"];
+if (favorite_fruits.includes("Mango")) {
+    console.log("I really like Mangos");
+}
+if (favorite_fruits.includes("Strawberry")) {
+    console.log("I really like Strawberrys");
+}
+if (favorite_fruits.includes("Apple")) {
+    console.log("i really like Apples");
+}
+if (favorite_fruits.includes("Orange")) {
+    console.log("I really like Orange");
+}
+if (favorite_fruits.includes("Banana")) {
+    console.log("I really like Banana");
+}
