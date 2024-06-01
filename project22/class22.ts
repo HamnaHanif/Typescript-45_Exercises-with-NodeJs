b4f1bc228b1359b2418a8970ae8199071f9c0517
HamnaHanif <hanifhamna5@gmail.com>
2024-06-01 15:21:55 -0700

    Add files via upload

---
+++ project22/class22.ts
@@ -0,0 +1,14 @@
+//ASSIGNMENT NO 22 :
+//Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make onehappen.
+// Change an index in one of your programs to produce an index error.
+// Make sure you correct the error before closing the program.
+
+
+//Creating a Array
+let errorArray: string[] = ["A", "B", "C", "D","E", "F", "G", "H"];
+
+//Producing Error !  by Accessing invalid index of Array
+//console.log(errorArray[9]);
+
+//Error Removed
+console.log(errorArray[2]);
