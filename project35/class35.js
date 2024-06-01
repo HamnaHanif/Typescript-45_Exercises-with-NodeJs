7b1c7b3ebcd8223c4fbb5f2d674fc50f7a1094bb
HamnaHanif <hanifhamna5@gmail.com>
2024-06-01 15:30:18 -0700

    Add files via upload

---
+++ project35/class35.js
@@ -0,0 +1,8 @@
+var petAnimals = ["Cat", "Dog", "Rabbit"];
+// Using for-Loop
+for (var _i = 0, petAnimals_1 = petAnimals; _i < petAnimals_1.length; _i++) {
+    var onePet = petAnimals_1[_i];
+    console.log("A ".concat(onePet, " would take a great pet."));
+}
+// Print a message outside of for-Loop
+console.log("Any of these animals would make a great pet!");
