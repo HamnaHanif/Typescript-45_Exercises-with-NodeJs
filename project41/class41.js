aea8684ffe222580ecd7c1e0d5c6e075828a2b93
HamnaHanif <hanifhamna5@gmail.com>
2024-06-01 15:33:56 -0700

    Add files via upload

---
+++ project41/class41.js
@@ -0,0 +1,8 @@
+// Define a function to print each magnician name from an Array
+function show_magnicians(magnicians) {
+    magnicians.forEach(function (name) { return console.log(name); });
+}
+//Define an array containing magnicians name
+var magnician_names = ["Harry Porter", "Mifra", "Wania"];
+//Call the function to print each magnician names 
+show_magnicians(magnician_names);
---
+++ project41/class41.ts
@@ -0,0 +1,10 @@
+// Define a function to print each magnician name from an Array
+function show_magnicians(magnicians: string[]){
+    magnicians.forEach(name => console.log(name));
+}
+
+//Define an array containing magnicians name
+let magnician_names = ["Harry Porter", "Mifra", "Wania"];
+
+//Call the function to print each magnician names 
+show_magnicians(magnician_names);
