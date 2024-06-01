5fc5415bce0dfbe782e59c8481364abb54b7e11e
HamnaHanif <hanifhamna5@gmail.com>
2024-06-01 15:08:29 -0700

    Add files via upload

---
+++ 3rd project/3class.js
@@ -0,0 +1,10 @@
+//ASSIGNMENT NO 3:
+//Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, 
+//uppercase, and titlecase.
+var personName = "Hamna Hanif";
+//LowerCase :
+console.log(personName.toLowerCase());
+//UpperCase :
+console.log(personName.toUpperCase());
+//TitleCase :
+console.log(personName.replace(/\bw/g, function (c) { return c.toUpperCase(); }));
