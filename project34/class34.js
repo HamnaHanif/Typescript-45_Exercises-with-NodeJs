befa743bf06c14881222e8b88e79af7966152e18
HamnaHanif <hanifhamna5@gmail.com>
2024-06-01 15:29:48 -0700

    Add files via upload

---
+++ project34/class34.js
@@ -0,0 +1,8 @@
+var pizza = ["Chicken Tika", "Malai Cheese", "Fajita"];
+//Using for-Loop
+for (var _i = 0, pizza_1 = pizza; _i < pizza_1.length; _i++) {
+    var onePizza = pizza_1[_i];
+    console.log("I like ".concat(onePizza, " pizza"));
+}
+//Print a Message outside of the for-Loop
+console.log("Pizza is Love");
