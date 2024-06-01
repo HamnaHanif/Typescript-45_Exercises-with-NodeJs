1afbc154933ce843db4b593baeb72a27ce2ac3ad
HamnaHanif <hanifhamna5@gmail.com>
2024-06-01 15:12:04 -0700

    Add files via upload

---
+++ project9/CLASS9.TS.js
@@ -0,0 +1,6 @@
+//ASSIGNMENT NO 9 :
+//Favorite Number: Store your favorite number in a variable.
+// Then, using that variable, create a message that reveals your favorite number. Print that message.
+var favNumber = 27;
+var message = "Mine Favorite Number is:";
+console.log(message, favNumber);
---
+++ project9/class9.ts
@@ -0,0 +1,8 @@
+//ASSIGNMENT NO 9 :
+//Favorite Number: Store your favorite number in a variable.
+// Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+
+let favNumber = 27;
+let message = "Mine Favorite Number is:";
+console.log(message, favNumber);
