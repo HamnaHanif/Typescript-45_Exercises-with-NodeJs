652caf405da1b5bea59ed1c405aab344af040b8f
HamnaHanif <hanifhamna5@gmail.com>
2024-06-01 15:33:25 -0700

    Add files via upload

---
+++ project40/class40.js
@@ -0,0 +1,20 @@
+// Define the make_album function
+ function make_album(artist_name, album_title, tracks) {
+         var album = {
+       artist: artist_name,
+         title: album_title,
+     };
+    if (tracks !== undefined) {
+         album.tracks = tracks;
+     }
+     return album;
+ }
+ // Calling three functions and creating 3 variable with different values
+ var album1 = make_album("Hamna", "Album title 1");
+ var album2 = make_album("Haroon", "Album title 2");
+ var album3 = make_album("Rafia", "Album title 3", 10);
+ 
+ //Print values of our object created by function
+ console.log(album1);
+ console.log(album2);
+ console.log(album3);
