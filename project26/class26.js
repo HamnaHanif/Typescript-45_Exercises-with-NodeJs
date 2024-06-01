9adf86084329dddabcb45ce01bb5575500111ffa
HamnaHanif <hanifhamna5@gmail.com>
2024-06-01 15:24:19 -0700

    Add files via upload

---
+++ project26/class26.js
@@ -0,0 +1,15 @@
+// Define Variable
+var alienColor = "Green";
+if (alienColor === "Green") {
+    console.log("player just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("player just earned 10 points.");
+}
+//Second Version
+if (alienColor === "Blue") {
+    console.log("player just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("Player just earned 10 points for shooting the alien");
+}
