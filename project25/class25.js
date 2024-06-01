b431dfe8b2a51e03482f1b16a5264b04b85f5230
HamnaHanif <hanifhamna5@gmail.com>
2024-06-01 15:23:45 -0700

    Add files via upload

---
+++ project25/class25.js
@@ -0,0 +1,7 @@
+var alienColor = "Yellow";
+if (alienColor === "Yellow") {
+    console.log("Alien color is Yellow Players just earned 5 points.");
+}
+if (alienColor === "Red") {
+    console.log("Alien color is red");
+}
