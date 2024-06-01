56abce8ed28afa7a0ceb4bacca57a2eec07a5a14
HamnaHanif <hanifhamna5@gmail.com>
2024-06-01 15:30:50 -0700

    Add files via upload

---
+++ project36/class36.js
@@ -0,0 +1,5 @@
+function make_shirt(size, printMessage) {
+    console.log("You selected ".concat(size, " size shirt with ").concat(printMessage, " prints on shirt"));
+}
+make_shirt("Medium", "lucky Star");
+make_shirt("Small", "Hamna");
