fe174aa15529c53034ab7a4042bc804bddbfc6d4
HamnaHanif <hanifhamna5@gmail.com>
2024-06-01 15:19:37 -0700

    Add files via upload

---
+++ project19/class19.ts
@@ -0,0 +1,11 @@
+//ASSIGNMENT NO 19 :
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating 
+//the number of people you are inviting to dinner.
+
+
+var guestList = ["Rabeea", "Fazila", "Mariyam", "Umaima"];
+//guestList.forEach(function (oneGuest) { return console.log("Salam ".concat(oneGuest, ", would you like to dinner with me")); });
+
+let lengthGuests: number = guestList.length;
+
+console.log(`We are Inviting total ${lengthGuests} guests.`);
