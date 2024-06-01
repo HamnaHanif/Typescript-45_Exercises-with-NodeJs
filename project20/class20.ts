c4dd5af6beaf1df6ddf9491762823223f339c8d6
HamnaHanif <hanifhamna5@gmail.com>
2024-06-01 15:20:15 -0700

    Add files via upload

---
+++ project20/class20.ts
@@ -0,0 +1,12 @@
+//ASSIGNMENT NO 20:
+//Think of something you could store in a array. For example, you could make a list of mountains, rivers,
+// countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+
+//Making a Programming Languages Array
+let programmingLanguages: string[] = ["Typescript", "Javascript", "Python", "Html", "CSS", "JAVA", "PHP"];
+//Print the message of list
+console.log("List of Programming Languages:");
+//print the values of array in the form of list
+programmingLanguages.forEach(language => console.log(language));
+
